6a28adb23b57bf09986645a60a9aab54a8737da8
Eda <edda.yavuz@gmail.com>
2023-11-28 08:56:23 +0200

    Update Main.java

--- calculatorExample/src/Main.java
+++ calculatorExample/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         calculate calc = new calculate(-241, 2); // create a calculator object called "calc"
-        System.out.println(calc.getDiv());
-
+        System.out.println(calc.getSum());
+        System.out.println(calc.getSub());
     }
 }
