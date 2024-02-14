60136190e85594d27f498d0c027b6c385d59b356
Eda <edda.yavuz@gmail.com>
2024-02-14 18:05:33 +0200

    Create empty gradebook.java

---
+++ arrays/gradebook.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class gradebook {
+    public static void main(String[] args) {
+        String[] students = {"Nikoleta", "Vasil", "Martin","Svetoslav","Irina", "Aleksandar"};
+        int[] grades = {99, 49, 89, 45, 35, 78 };
+
+        /* Implement a switch case for the menu: 
+        * 1. Calculate Average
+        * 2. Display the student with the highest grade
+        * 3. Display the student with the lowest grade
+        * 4. Display the count of students who are failing (>50 is failing)
+        * 5. Look up student grade with user input. 
+        * 6. Exit. */   
+    }
+
+    private static double calculateAverage(int[] grades) {
+      
+    }
+
+    private static String findHighestGradeStudent(String[] studentNames, int[] grades) {
+      
+    }
+
+    private static String findLowestGradeStudent(String[] studentNames, int[] grades) {
+      
+    }
+
+    private static int countFailedStudents(int[] grades) {
+      
+    }
+
+    private static int lookupStudentGrade(String[] studentNames, int[] grades, String studentToLookup) {
+       
+    }
+}
