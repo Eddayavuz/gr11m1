6e27ff438a9defbe885ea85c9c3efddce18f9d4d
Eda <edda.yavuz@gmail.com>
2024-02-15 08:55:33 +0200

    Update arrayOperationsLectureNotes.java

--- arrays/arrayOperationsLectureNotes.java
+++ arrays/arrayOperationsLectureNotes.java
@@ -86,7 +86,7 @@ public class Main {
     public static boolean findDuplicates(int[] array) {
         boolean hasDuplicate = false;
         for (int i = 0; i < array.length; i++){
-            for (int j = 0; j < i; j++){
+            for (int j = i+1; j < array.length; j++){
                 if (array[i] == array[j])
                     hasDuplicate = true;
             }
