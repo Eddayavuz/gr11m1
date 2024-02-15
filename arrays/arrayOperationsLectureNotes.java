bdfb35207ffa1392e1f341cd1abd4bed969dc5bc
Eda <edda.yavuz@gmail.com>
2024-02-15 08:16:23 +0200

    Update arrayOperationsLectureNotes.java

--- arrays/arrayOperationsLectureNotes.java
+++ arrays/arrayOperationsLectureNotes.java
@@ -1,42 +1,96 @@
-public class arrayOperationsLectureNotes {
+public class Main {
     public static void main(String[] args) {
+        int[] arr = {40,21,42,53,12,14,9,21};
 
-        // 1. FINDING THE BIGGEST NUMBER IN ARRAY
-        int[] arr = {40,21,42,53,12,14,9};
-        int max = 0;
-        for (int i = 0; i < arr.length; i++){
-            if(arr[i] > max){
-                max = arr[i];
+        // 1. FINDING THE SUM OF ELEMENTS IN ARRAY
+        System.out.println(sumOfItems(arr));
+
+        // 2. FINDING A NUMBER IN AN ARRAY
+        System.out.println(numberFinder(53, arr));
+
+        // 3. CREATE AN ARRAY WITH ODD NUMBERS WITHIN A RANGE
+        createOddArray(10);
+
+        // 4. FINDING THE BIGGEST NUMBER IN ARRAY
+        System.out.println(findMax(arr));
+
+        // 5. FINDING THE AVERAGE OF ARRAY
+        System.out.println(calculateAverage(arr));
+
+        // 6. COUNTING EVEN NUMBERS IN ARRAY
+        System.out.println(countEvenNumbers(arr));
+
+        // 7. FINDING DUPLICATES
+        System.out.println(findDuplicates(arr));
+    }
+
+    public static int sumOfItems(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+
+    public static boolean numberFinder(int num, int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == num) {
+                return true;
             }
         }
-        System.out.println(max);
+        return false;
+    }
 
-        // 2. FINDING THE AVERAGE OF ARRAY
-        int[] arr2 = {40,21,42,53,12,14,9};
-        int sum = 0;
-        for (int i = 0; i < arr2.length; i++){
-           sum += arr2[i];
+    public static void createOddArray(int size) {
+        int[] oddArray = new int[size];
+        int oddNumber = 1;
+
+        for (int i = 0; i < size; i++) {
+            oddArray[i] = oddNumber;
+            oddNumber += 2;
         }
-        System.out.println(sum/arr2.length);
+        System.out.print("Odd Array: ");
+        for (int i = 0; i < oddArray.length; i++) {
+            System.out.print(oddArray[i] + " ");
+        }
+        System.out.println();
+    }
 
-        // 3. COUNTING EVEN NUMBERS IN ARRAY
-        int[] arr3 = {40,21,42,53,12,14,9};
+    public static int findMax(int[] array) {
+        int max = 0;
+        for (int i = 0; i < array.length; i++){
+            if(array[i] > max){
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    public static int calculateAverage(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++){
+            sum += array[i];
+        }
+        return sum / array.length;
+    }
+
+    public static int countEvenNumbers(int[] array) {
         int count = 0;
-        for (int i = 0; i < arr3.length; i++){
-            if(arr[i]%2 == 0)
+        for (int i = 0; i < array.length; i++){
+            if(array[i] % 2 == 0)
                 count++;
         }
-        System.out.println(count);
+        return count;
+    }
 
-        // 4. FINDING DUPLICATES
-        int[] arr4 = {40,21,42,53,46,65,40};
+    public static boolean findDuplicates(int[] array) {
         boolean hasDuplicate = false;
-        for (int i=0; i<arr4.length; i++){
-            for (int j=0; j<i; j++){
-                if (arr4[i] == arr4[j])
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j < i; j++){
+                if (array[i] == array[j])
                     hasDuplicate = true;
             }
         }
-        System.out.println(hasDuplicate);
+        return hasDuplicate;
     }
 }
