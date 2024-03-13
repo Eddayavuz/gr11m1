ba40e6d98920cf603b8342c7ce693bb0da94bf26
Eda <edda.yavuz@gmail.com>
2024-03-13 17:31:22 +0200

    Update occuranceCounter.java

--- recursion/occuranceCounter.java
+++ recursion/occuranceCounter.java
@@ -1,16 +1,1 @@
-public class occuranceCounter {
-    public static void main(String[] args) {
-    int[] array = {5,3,2,1,2,3,4};
-    System.out.println(howMany(array, 2, array.length-1, 0));
 
-    }
-    public static int howMany(int[] arr, int target, int index, int count) {
-        if (index < 0)
-            return count;
-
-        if(arr[index] == target)
-            return count + 1 + howMany(arr, target, index-1, count);
-
-        return howMany(arr, target, index-1, count);
-    }
-}
