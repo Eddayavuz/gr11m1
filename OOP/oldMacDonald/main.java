18b1382f2f690da64d25b04b26e33ef43fe340c3
Eda <edda.yavuz@gmail.com>
2024-09-27 15:46:47 +0300

    Create main.java

---
+++ OOP/oldMacDonald/main.java
@@ -0,0 +1,38 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Old MacDonald had a farm");
+        System.out.println("E-I-E-I-O");
+        System.out.println("And on that farm they had a cow.");
+        System.out.println("E-I-E-I-O");
+        System.out.println("With a moo moo here and a moo moo there");
+        System.out.println("Here a moo, there a moo, everywhere a moo moo");
+        System.out.println("Old MacDonald had a farm");
+        System.out.println("E-I-E-I-O");
+
+    }
+/*
+    public static void intro()
+    {
+        System.out.println("Old MacDonald had a farm");
+        chorus();
+    }
+
+    public static void chorus()
+    {
+        System.out.println("E-I-E-I-O");
+    }
+
+    public static void verse(String animal, String sound)
+    {
+        System.out.println("And on this farm, they had a " + animal);
+        chorus();
+        System.out.println("With a " + sound + " " + sound
+                + " here and a " + sound + " " + sound + " there");
+        System.out.println("Here a " + sound
+                + ", there a " + sound
+                + ", everywhere a " + sound + " " + sound);
+    }
+*/
+}
