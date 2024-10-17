807edc8823ba32638d7d5d82f4e13a24b1d33fa3
Eda <edda.yavuz@gmail.com>
2024-10-17 17:40:23 +0300

    Update whileloops.java

--- Loops/whileloops.java
+++ Loops/whileloops.java
@@ -1,5 +1,3 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
@@ -12,7 +10,7 @@ public class Main {
             //code to be executed
            Increment / decrement statement
         }
-*/
+         
         // write num on the console as long as num is smaller than 10.
        //int i = 1;
         //while (i<10){
@@ -28,38 +26,64 @@ public class Main {
         //}
 
 
-
         // let's write a small program to print out the numbers between two entered numbers.
-        /*Scanner scanner = new Scanner(System.in);
+        
+        Scanner scanner = new Scanner(System.in);
         System.out.println("enter the start number");
         int start = scanner.nextInt();
         System.out.println("enter the end number");
         int end = scanner.nextInt();
 
-        while (start>=end){
-
-            if (start%2 == 0) {
-                System.out.println(start);
-            }
-            start--;
-        }*/
+        while (end>=start){
+            System.out.println(start);
+            end--;
+        }
 
         // let's modify this program to print out only the even numbers.
-        //while (start <= end){
-          //  if( start % 2 == 0){
-            //System.out.println(start);
-            //}
-            //start++;
-        //}
+        while (start <= end){
+          if( start % 2 == 0){
+            System.out.println(start);
+            }
+            start++;
+        }
         // let's write a small program to add numbers from one to ten and print out the result.
-       /*int num = 1;
+       int num = 1;
        int sum = 0;
         while (num <= 10) {
           sum = sum + num;
           num++; // inc
        }
         System.out.println("Sum of numbers from 1 to 10: " + sum); // TRY: print the result inside the while loop.
-*/
+
+
+  // do-while Loop
+        The do/while loop is a variant of the while loop.
+        This loop will execute the code block once, before checking if the condition is true,
+        then it will repeat the loop as long as the condition is true.
+
+        SYNTAX
+        do {
+            // code block to be executed
+            }
+        while (condition);
+
+        // EXAMPLE: Take a look at the code below, try to understand what it does.
+        int secretNumber = 7;
+        int guess;
+
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.print("Guess a number between 1 and 10: ");
+            guess = scanner.nextInt();
+
+            if (guess != secretNumber) {
+                System.out.println("Wrong guess! Try again.");
+            }
+        } while (guess != secretNumber);
+
+        System.out.println("Congratulations! You guessed the right number.");
+        */
 
         // STRING MANIPULATION - Reverse String
         String name = "EDA";
