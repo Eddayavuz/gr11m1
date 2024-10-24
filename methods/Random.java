248bdfc30c7a349e2d7df2cf1bcb0802abef9b2d
Eda <edda.yavuz@gmail.com>
2024-10-24 23:12:30 +0300

    Update Random.java

--- methods/Random.java
+++ methods/Random.java
@@ -1,15 +1,13 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         int i = 0;
-        Random rand = new Random();
+        Random rand = new Random(); // create random object.
         int randomNumber = 0;
 
         while(i<5){
-            randomNumber = rand.nextInt(100);
-            System.out.println(randomNumber);
+            randomNumber = rand.nextInt(100) + 1; // generate a random number within a range.
+            System.out.println(randomNumber); 
             i++;
         }
         }
