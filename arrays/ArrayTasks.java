223ed997e33cc922208d28ad10b265cef9f420c3
Eda <edda.yavuz@gmail.com>
2026-01-28 15:43:32 +0200

    Create ArrayTasks.java

---
+++ arrays/ArrayTasks.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Create an integer array with 10 elements
+
+        System.out.println(sumOfItems(arr));
+        System.out.println(calculateAverage(arr));
+        System.out.println(countEvenNumbers(arr));
+    }
+  
+    // TASK 1: Calculates and returns the sum of all elements in the array
+    public static int sumOfItems(int[] array) {
+        return 0;
+    }
+
+    // TASK 2: Calculates and returns the average of array elements
+    public static double calculateAverage(int[] array) {
+       return 0.0;
+    }
+
+    // TASK 3: Counts how many even numbers exist in the array
+    public static int countEvenNumbers(int[] array) {
+        return 0;
+    }
+
+}
